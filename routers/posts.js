c6865363c3e4c1e48dd053f7eb749713e8377aef
OtakeYuto <yuto.programming@gmail.com>
2023-10-16 02:00:55 +0900

    add some

---
+++ routers/posts.js
@@ -0,0 +1,55 @@
+const router = require("express").Router();
+const { PrismaClient } = require("@prisma/client");
+const isAuthenticated = require("../middlewares/isAuthenticated");
+
+const prisma = new PrismaClient();
+
+// つぶやき投稿API
+router.post("/post", isAuthenticated, async (req, res) => {
+  const { content } = req.body;
+
+  if (!content) {
+    return res.status(400).json({ message: "投稿内容がありません" });
+  }
+
+  try {
+    const newPost = await prisma.post.create({
+      data: {
+        content,
+        authorId: req.userId,
+      },
+      include: {
+        author: {
+          include: {
+            profile: true,
+          },
+        },
+      },
+    });
+    res.status(201).json(newPost);
+  } catch (err) {
+    res.status(500).json({ message: "サーバエラーです" });
+  }
+});
+
+// 最新つぶやき取得API
+router.get("/get_latest_post", async (req, res) => {
+  try {
+    const latestPosts = await prisma.post.findMany({
+      take: 10,
+      orderBy: { createdAt: "desc" },
+      include: {
+        author: {
+          include: {
+            profile: true,
+          },
+        },
+      },
+    });
+    return res.json(latestPosts);
+  } catch (err) {
+    res.status(500).json({ message: "サーバエラーです" });
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 const authRoute = require("./routers/auth");
+const postsRoute = require("./routers/posts");
+const usersRoute = require("./routers/users");
+const cors = require("cors");
 
 const app = express();
 const PORT = 5555;
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoute);
-
+app.use("/api/posts", postsRoute);
+app.use("/api/users", usersRoute);
 app.listen(PORT, () => console.log(`server is running on PORT ${PORT}`));
